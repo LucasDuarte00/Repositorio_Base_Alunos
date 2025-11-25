f3dff9450b1cf59ed0757fa0d8d8abc3ffa2912f
Duarte <lucasduartedias46@gmail.com>
2025-11-25 13:26:37 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/validacao_senha.py
@@ -0,0 +1,13 @@
+import json
+
+
+with open('senha.json', 'r', encoding='utf-8') as arquivo:
+      dados = json.load(arquivo)
+      
+email = input('Digite o seu email: ')
+senha = input('Digite a sua senha: ')
+
+if email == dados['email'] and senha == dados['senha']:
+    print('Acesso liberado!')
+else:
+    print('Acesso negado!')
